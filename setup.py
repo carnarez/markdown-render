2829c8885af8e6b033bd8f88e7ebfec571a42cf8
Clément Arnarez <11967125+carnarez@users.noreply.github.com>
2023-10-25 10:05:36 +0200

    Refactor (#2)
    * Re-organised the stuff a tad
    * Removed useless .gitignore
    * Updated GitHub Actions YAML
    * Delete hidden file
    * Can't believe I have to add this stupid file...

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Make this little DIY installable (via `pip install git+https://...`)."""
+
+import setuptools
+
+setuptools.setup(
+    author="carnarez",
+    description=("Opinionated way to render `Markdown` into web pages."),
+    entry_points={"console_scripts": ["mdrender=markdown_render.render:cli"]},
+    install_requires=["markdown", "pyyaml"],
+    name="markdown-render",
+    packages=["markdown_render"],
+    package_data={"markdown_render": ["py.typed", "template.html"]},
+    url="https://github.com/carnarez/markdown-render",
+    version="0.0.1",
+)
